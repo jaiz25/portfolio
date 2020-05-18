fae8d725367cda29b69287fe333f0d8178afd4dd
jaiz25 <jaizelle@gmail.com>
2020-05-17 23:25:17 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
+
+setTimeout(function start (){
+
+  $('.bar').each(function(i){
+    var $bar = $(this);
+    $(this).append('<span class="count"></span>')
+    setTimeout(function(){
+      $bar.css('width', $bar.attr('data-percent'));
+    }, i*100);
+  });
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).parent('.bar').attr('data-percent')
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now) +'%');
+        }
+    });
+});
+
+}, 500)
